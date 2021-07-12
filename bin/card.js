30da558d501c88a4054d16ace7a1515f954b0ce6
Carlos Azaustre <cazaustre@gmail.com>
2021-07-12 08:00:59 +0200

    update headline

--- bin/card.js
+++ bin/card.js
@@ -8,16 +8,19 @@ const COLOR_THEME = '#ffb300';
 // Text + chalk definitions
 let data = {
   'name': chalk.white.bold('👋 Hi! I\'m Carlos Azaustre'),
+  'shortline': chalk.white.italic('JavaScript Lover | Developer Advocate on Web Technologies'),
   'handle': chalk.hex(COLOR_THEME)('@carlosazaustre'),
-  'work': chalk.white('💻 Web Development Instructor @ Fullstack JS Academy\n🎓 BSc, Telematics Engineering\n🏅 GDE in Web Technologies'),
+  'work': chalk.white('💻 Director of MSc. Fullstack Web Development @ Universidad Europea de Madrid'),
+  'book': chalk.white('📙 Author of ') + chalk.white.italic('Aprendiendo JavaScript - ') + chalk.hex(COLOR_THEME)(' https://carlosazaustre.es/libros/aprendiendo-javascript'),
+  'work_2': chalk.white('🎓 BSc, Telematics Engineering\n🏅 Google Developer Expert (GDE) in Web Technologies\n🚀 Auth0 Ambassador'),
 
-  'instagram': chalk.hex(COLOR_THEME)('https://instagram.com/carlosazaustre'),
-  'youtube': chalk.hex(COLOR_THEME)('https://youtube.com/carlosazaustre'),
-  'twitch': chalk.hex(COLOR_THEME)('https://twitch.tv/carlosazaustre'),
-  'twitter': chalk.hex(COLOR_THEME)('https://twitter.com/carlosazaustre'),
-  'github': chalk.hex(COLOR_THEME)('https://github.com/carlosazaustre'),
-  'linkedin': chalk.hex(COLOR_THEME)('https://linkedin.com/in/carlosazaustre'),
   'web': chalk.hex(COLOR_THEME)('https://carlosazaustre.es'),
+  'github': chalk.hex(COLOR_THEME)('https://github.com/carlosazaustre'),
+  'youtube': chalk.hex(COLOR_THEME)('https://youtube.com/carlosazaustre'),
+  'twitter': chalk.hex(COLOR_THEME)('https://twitter.com/carlosazaustre'),
+  'instagram': chalk.hex(COLOR_THEME)('https://instagram.com/carlosazaustre'),
+  'twitch': chalk.hex(COLOR_THEME)('https://twitch.tv/carlosazaustre'),
+  'linkedin': chalk.hex(COLOR_THEME)('https://linkedin.com/in/carlosazaustre'),
   'npx': chalk.white('npx carlosazaustre'),
 
   'labelInstagram': chalk.white.bold('   Instagram:'),
@@ -33,15 +36,21 @@ let data = {
 const output = `
 ${data.name} (${data.handle})
 
-${data.work}
+${data.shortline}
 
-${data.labelInstagram}  ${data.instagram}
+${data.work}
+${data.book}
+${data.work_2}
+
+${data.labelWeb}  ${data.web}
+
 ${data.labelYoutube}  ${data.youtube}
-${data.labelTwitch}  ${data.twitch}
 ${data.labelTwitter}  ${data.twitter}
+${data.labelInstagram}  ${data.instagram}
+${data.labelTwitch}  ${data.twitch}
+
 ${data.labelGitHub}  ${data.github}
 ${data.labelLinkedIn}  ${data.linkedin}
-${data.labelWeb}  ${data.web}
 
 ${data.labelCard}  ${data.npx}
 
